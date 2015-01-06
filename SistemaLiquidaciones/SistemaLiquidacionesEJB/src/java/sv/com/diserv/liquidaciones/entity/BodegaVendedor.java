9432a241d1a5fbdbb69da68b2ca9e3a21fde6557
n75acosta <n75acosta@gmail.com>
2015-01-06 14:49:10 +0000

    Actualizacion mantenimiento de entidad persona By SGGG

--- SistemaLiquidaciones/SistemaLiquidacionesEJB/src/java/sv/com/diserv/liquidaciones/entity/BodegaVendedor.java
+++ SistemaLiquidaciones/SistemaLiquidacionesEJB/src/java/sv/com/diserv/liquidaciones/entity/BodegaVendedor.java
@@ -9,11 +9,14 @@ import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -34,6 +37,8 @@ public class BodegaVendedor implements Serializable {
     @Basic(optional = false)
     @NotNull
     @Column(name = "ID")
+    @SequenceGenerator(name = "SEQ_IDBODEGAVEND", sequenceName = "SEQ_IDBODEGAVEND")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_IDBODEGAVEND" )
     private Integer id;
     @JoinColumn(name = "IDBODEGA", referencedColumnName = "IDBODEGA")
     @ManyToOne(optional = false)
