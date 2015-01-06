cde3adc3479a26af3af1f43b452404c258b570d6
n75acosta <n75acosta@gmail.com>
2015-01-06 17:10:10 +0000

    Actualizacion mantenimiento de entidad persona By SGGG

--- SistemaLiquidaciones/SistemaLiquidacionesEJB/src/java/sv/com/diserv/liquidaciones/dto/OperacionesBodegaVendedorDTO.java
+++ SistemaLiquidaciones/SistemaLiquidacionesEJB/src/java/sv/com/diserv/liquidaciones/dto/OperacionesBodegaVendedorDTO.java
@@ -1,25 +1,31 @@
 package sv.com.diserv.liquidaciones.dto;
 
-import sv.com.diserv.liquidaciones.entity.Personas;
-
+import sv.com.diserv.liquidaciones.entity.BodegaVendedor;
 /**
  *
  * @author sonia.garcia
  */
 public class OperacionesBodegaVendedorDTO extends BaseResponse {
 
-    private Personas persona;
+    private BodegaVendedor bodegaVendedor;
 
     public OperacionesBodegaVendedorDTO(Integer codigoRespuesta, String mensajeRespuesta) {
         super(codigoRespuesta, mensajeRespuesta);
     }
 
-    public Personas getPersona() {
-        return persona;
+    /**
+     * @return the bodegaVendedor
+     */
+    public BodegaVendedor getBodegaVendedor() {
+        return bodegaVendedor;
     }
 
-    public void setPersona(Personas persona) {
-        this.persona = persona;
+    /**
+     * @param bodegaVendedor the bodegaVendedor to set
+     */
+    public void setBodegaVendedor(BodegaVendedor bodegaVendedor) {
+        this.bodegaVendedor = bodegaVendedor;
     }
 
+   
 }
