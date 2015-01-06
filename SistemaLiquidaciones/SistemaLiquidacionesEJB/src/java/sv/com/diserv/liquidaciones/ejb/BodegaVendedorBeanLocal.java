c8ae7a561f22bac2a18297cbef79b0e02cf5cefa
n75acosta <n75acosta@gmail.com>
2015-01-06 17:12:11 +0000

    Actualizacion mantenimiento de entidad persona By SGGG

--- SistemaLiquidaciones/SistemaLiquidacionesEJB/src/java/sv/com/diserv/liquidaciones/ejb/BodegaVendedorBeanLocal.java
+++ SistemaLiquidaciones/SistemaLiquidacionesEJB/src/java/sv/com/diserv/liquidaciones/ejb/BodegaVendedorBeanLocal.java
@@ -7,8 +7,8 @@ package sv.com.diserv.liquidaciones.ejb;
 
 import java.util.List;
 import javax.ejb.Local;
-import sv.com.diserv.liquidaciones.dto.BusquedaBodegaDTO;
 import sv.com.diserv.liquidaciones.dto.OperacionesBodegaVendedorDTO;
+import sv.com.diserv.liquidaciones.entity.BodegaVendedor;
 import sv.com.diserv.liquidaciones.entity.Bodegas;
 import sv.com.diserv.liquidaciones.exception.DiservBusinessException;
 
@@ -19,30 +19,13 @@ import sv.com.diserv.liquidaciones.exception.DiservBusinessException;
 @Local
 public interface BodegaVendedorBeanLocal {
 
-    /**
-     * *
-     *
-     * @param inicio
-     * @param fin
-     * @return
-     * @throws DiservBusinessException
-     */
+    
     public List<Bodegas> loadAllBodegasAsignables() throws DiservBusinessException;
-
-    /**
-     * *
-     *
-     * @return @throws DiservBusinessException
-     */
     public Integer countAllBodegasAsignables() throws DiservBusinessException;
-
-    /**
-     * *
-     *
-     * @param bodega
-     * @return
-     * @throws DiservBusinessException
-     */
     public OperacionesBodegaVendedorDTO asignarBodega(Bodegas bodega) throws DiservBusinessException;
+    public OperacionesBodegaVendedorDTO desasignarBodega(BodegaVendedor bodegaVendedor) throws DiservBusinessException ;
+    public Bodegas findBodegaByID(Integer idBodega) throws DiservBusinessException;
+    public Bodegas findBodegaAsignada(Integer idVendedor) throws DiservBusinessException;
+    public BodegaVendedor findBodegaVendedorByIdVendedorBodega(Integer idVendedor, Integer idBodega) throws DiservBusinessException;
 
 }
