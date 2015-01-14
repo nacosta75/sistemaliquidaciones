e1d66af38fa0ebda5b38881c282778c250f3006d
sonia.guadalupe.garcia <sonia.guadalupe.garcia@gmail.com>
2015-01-14 20:08:17 +0000

    Almacenamiento de asignaciones

--- SistemaLiquidaciones/SistemaLiquidacionesEJB/src/java/sv/com/diserv/liquidaciones/ejb/MovimientosBeanLocal.java
+++ SistemaLiquidaciones/SistemaLiquidacionesEJB/src/java/sv/com/diserv/liquidaciones/ejb/MovimientosBeanLocal.java
@@ -21,88 +21,12 @@ import sv.com.diserv.liquidaciones.exception.DiservBusinessException;
 @Local
 public interface MovimientosBeanLocal {
 
-    /**
-     * *
-     *
-     * @param inicio
-     * @param fin
-     * @param tipo
-     * @return
-     * @throws DiservBusinessException
-     */
     public List<Movimientos> loadAllMovimientos(int inicio, int fin, int tipo) throws DiservBusinessException;
-
-    /**
-     * *
-     *
-     * @param tipoMovimiento
-     * @return
-     * @throws DiservBusinessException
-     */
     public Integer countAllMovimientos(int tipoMovimiento) throws DiservBusinessException;
-
-    /**
-     * *
-     *
-     * @param movimiento
-     * @return
-     * @throws DiservBusinessException
-     */
     public OperacionesMovimientoDTO guardarMovimiento(Movimientos movimiento) throws DiservBusinessException;
-
-    /**
-     * **
-     *
-     * @param nombreLike
-     * @return
-     * @throws DiservBusinessException
-     */
-    public List<Personas> loadAllPersonasByLike(String nombreLike) throws DiservBusinessException;
-
-    /**
-     * *
-     *
-     * @param movimiento
-     * @return
-     * @throws DiservBusinessException
-     */
     public OperacionesMovimientoDTO actualizarMovimiento(Movimientos movimiento) throws DiservBusinessException;
-
-    /**
-     * **
-     *
-     * @param movimiento
-     * @return
-     * @throws DiservBusinessException
-     */
     public OperacionesMovimientoDTO eliminarMovimiento(Movimientos movimiento) throws DiservBusinessException;
-
-    /**
-     * *
-     *
-     * @param request
-     * @return
-     * @throws DiservBusinessException
-     */
-    public List<Personas> buscarPersonaByCriteria(BusquedaPersonaDTO request) throws DiservBusinessException;
-
-    /**
-     * **
-     *
-     * @param idVendedor
-     * @param tipoMov
-     * @return
-     * @throws DiservBusinessException
-     */
     public Integer maxNumDocByVendedorAndTipoMov(int idVendedor, int tipoMov) throws DiservBusinessException;
-
-    /**
-     * **
-     *
-     * @param codigoMovimiento
-     * @return
-     * @throws DiservBusinessException
-     */
     public List<MovimientosDet> loadDetalleMovimientoByIdMovimento(Integer codigoMovimiento) throws DiservBusinessException;
-
+    public Movimientos findMovimientoById( Integer idMovimiento) throws DiservBusinessException;
 }
