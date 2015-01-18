380161b8dd187bcb5f63ee1ebe5e41bfb6ea0a33
sonia.guadalupe.garcia <sonia.guadalupe.garcia@gmail.com>
2015-01-18 06:42:42 +0000

    Almacenamiento de asignaciones

--- SistemaLiquidaciones/SistemaLiquidacionesWAR/src/java/sv/com/diserv/web/ui/asignaciones/render/LotesItemRenderer.java
+++ SistemaLiquidaciones/SistemaLiquidacionesWAR/src/java/sv/com/diserv/web/ui/asignaciones/render/LotesItemRenderer.java
@@ -1,39 +1,38 @@
 package sv.com.diserv.web.ui.asignaciones.render;
 
-import java.text.SimpleDateFormat;
-import sv.com.diserv.web.ui.asignaciones.render.*;
-import org.zkoss.zk.ui.sys.ComponentsCtrl;
-
 import org.zkoss.zul.Listcell;
 import org.zkoss.zul.Listitem;
 import org.zkoss.zul.ListitemRenderer;
-import sv.com.diserv.liquidaciones.entity.LotesExistencia;
+import sv.com.diserv.liquidaciones.dto.ConsolidadoAsignacionesDTO;
 
 public class LotesItemRenderer implements ListitemRenderer {
 
-    LotesExistencia lote;
+    ConsolidadoAsignacionesDTO lote;
 
     @Override
     public void render(Listitem item, Object data) throws Exception {
-        lote = (LotesExistencia) data;
-        SimpleDateFormat formateador = new SimpleDateFormat("dd/MM/yyyy");
+        lote = (ConsolidadoAsignacionesDTO) data;
 
-        Listcell lb= new Listcell(String.valueOf(lote.getIdlote()));
+        Listcell lb= new Listcell(String.valueOf(lote.getLote().getIdlote()));
         lb.setParent(item);
         
-        lb= new Listcell(String.valueOf(lote.getIdarticulo().getIdarticulo()));
+        lb= new Listcell(String.valueOf(lote.getLote().getIdarticulo().getIdarticulo()));
         lb.setParent(item);
         
-        lb = new Listcell(String.valueOf(lote.getIcc()));
+        lb = new Listcell(String.valueOf(lote.getLote().getIcc()));
         lb.setParent(item);
         
-        lb = new Listcell(String.valueOf(lote.getImei()));
+        lb = new Listcell(String.valueOf(lote.getLote().getImei()));
         lb.setParent(item);
         
-        lb = new Listcell(String.valueOf(lote.getTelefono()));
+        lb = new Listcell(String.valueOf(lote.getLote().getTelefono()));
         lb.setParent(item);
 
         item.setAttribute("data", data);
+        
+        if(lote.getSelected())
+            item.setSelected(true);
+        
 
 //        ComponentsCtrl.applyForward(item, "onDoubleClick=onDoubleClickedAsignacion");
 //        ComponentsCtrl.applyForward(item, "onClick=onClickedAsignacion");
