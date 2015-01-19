a618d52536c38f06061724604a9ed217b95f0497
n75acosta <n75acosta@gmail.com>
2015-01-19 21:24:28 +0000

    cambios articulos

--- SistemaLiquidaciones/SistemaLiquidacionesEJB/src/java/sv/com/diserv/liquidaciones/ejb/DetListaPrecioBeanLocal.java
+++ SistemaLiquidaciones/SistemaLiquidacionesEJB/src/java/sv/com/diserv/liquidaciones/ejb/DetListaPrecioBeanLocal.java
@@ -2,6 +2,7 @@ package sv.com.diserv.liquidaciones.ejb;
 
 import java.util.List;
 import javax.ejb.Local;
+import sv.com.diserv.liquidaciones.dto.OperacionesPreciosDTO;
 import sv.com.diserv.liquidaciones.entity.DetListaPrecio;
 import sv.com.diserv.liquidaciones.exception.DiservBusinessException;
 
@@ -13,9 +14,11 @@ import sv.com.diserv.liquidaciones.exception.DiservBusinessException;
 public interface DetListaPrecioBeanLocal {
 
     public DetListaPrecio findDetPrecioByIdArticulo(Integer idEncListaPrecio, Integer IdArticulo) throws DiservBusinessException;    
-
     
     // retorna los precios del articulo
     public List<DetListaPrecio> listDetPrecioByIdArticulo(Integer IdArticulo,Integer inicio,Integer fin) throws DiservBusinessException;    
+    
+    public OperacionesPreciosDTO guardarPrecio(DetListaPrecio detListaPrecio) throws DiservBusinessException;
+    public OperacionesPreciosDTO actualizarPrecio(DetListaPrecio detListaPrecio) throws DiservBusinessException;
 
 }
