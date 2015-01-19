07de4af53015d80541f0fa7dfd542f6cb8867188
n75acosta <n75acosta@gmail.com>
2015-01-19 21:31:43 +0000

    detalle precios

---
+++ SistemaLiquidaciones/SistemaLiquidacionesWAR/src/java/sv/com/diserv/web/ui/articulos/DetallePreciosCtrl.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.com.diserv.web.ui.articulos;
+
+import org.apache.log4j.Logger;
+import org.zkoss.zul.Window;
+import sv.com.diserv.web.ui.util.BaseController;
+
+/**
+ *
+ * @author abraham.acosta
+ */
+public class DetallePreciosCtrl extends BaseController{
+    
+    private static final Logger logger = Logger.getLogger(DetallePreciosCtrl.class.getName());
+    private static final long serialVersionUID = -546886879998950944L;
+    
+    
+    protected Window detallePreciosWindow;
+}
